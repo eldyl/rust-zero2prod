7fa5cceb39a9cadb77f97018bed1212ececfb2ec
Dylan Ullrich <dylan@eldyl.org>
2025-04-29 16:27:46 -0700

    Add telemetry with tracing crate

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,29 @@
+use secrecy::ExposeSecret;
 use sqlx::PgPool;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
+use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::main]
-async fn main() -> Result<(), std::io::Error> {
+async fn main() -> std::io::Result<()> {
+    // Initialize logging
+    let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
+    init_subscriber(subscriber);
+
+    // Get configuration file values
     let configuration = get_configuration().expect("Failed to read configuration file.");
-    let connection_pool = PgPool::connect(&configuration.database.connection_string())
-        .await
-        .expect("Failed to connect to postgres.");
+
+    // Connect to database
+    let connection_pool =
+        PgPool::connect(configuration.database.connection_string().expose_secret())
+            .await
+            .expect("Failed to connect to postgres.");
+
+    // Set server address
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
-    run(listener, connection_pool)?.await
+    run(listener, connection_pool)?.await?;
+
+    Ok(())
 }
