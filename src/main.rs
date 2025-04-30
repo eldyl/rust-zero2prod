f4dd634694b82fcc34379d78e6e934b4a36eb3b1
Dylan Ullrich <dylan@eldyl.org>
2025-04-29 17:12:10 -0700

    Update configurations to allow local and prod specific

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,10 @@ async fn main() -> std::io::Result<()> {
             .expect("Failed to connect to postgres.");
 
     // Set server address
-    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let address = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    );
     let listener = TcpListener::bind(address)?;
     run(listener, connection_pool)?.await?;
 
